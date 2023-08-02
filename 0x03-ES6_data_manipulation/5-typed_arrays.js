624276951440cc5a1f07115b273b28be48ad708c
prosiscky <legendaryprosperity001@gmail.com>
2023-08-02 06:28:52 +0100

    corrected task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@
  * @param {Number} length - The length of the buffer.
  * @param {Number} position - The position to modify.
  * @param {Number} value - The value to be stored in the position.
- * @author Prosperity 
+ * @author Prosperity
  * @returns {DataView}
  */
 export default function createInt8TypedArray(length, position, value) {
