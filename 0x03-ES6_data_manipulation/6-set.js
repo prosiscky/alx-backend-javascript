e0f139b27ea0e4a493c06c9b113b30348a2110e9
prosiscky <legendaryprosperity001@gmail.com>
2023-08-02 05:27:50 +0100

    A function that returns a set from an array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Prosperity
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
